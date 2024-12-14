62009aaf117368c661c4275cc00ef8fb9bd22584
Gabe Susman <gabe.susman@gmail.com>
2024-12-14 12:06:27 -0700

    changed withRelay config field to be interpreted as a bool

--- cmd/argus-stream-engine-service/main.go
+++ cmd/argus-stream-engine-service/main.go
@@ -8,11 +8,12 @@ func main() {
 	rtmp.LogInfo("Initializing Argus Stream Engine...")
 
 	config := NewConfig()
-	if config.WithRelay == "TRUE" {
+	if config.WithRelay {
 		rtmp.LogInfo("Relay enabled.")
 		rtmp.LogInfo("Relay Ingress Address: " + config.RelayIngressAddress)
 		rtmp.LogInfo("Relay Egress Address: " + config.RelayEgressAddress)
 		rtmp.LogInfo("Egress Destination Address: " + config.EgressDestinationAddress)
+
 	}
 
 	rtmpServer := rtmp.NewServer()
