5d05b756eae6f7787502be69d53d9682ac75bead
Gabe Susman <gabe.susman@gmail.com>
2024-12-14 12:02:18 -0700

    integrated configuration with service executin

--- cmd/argus-stream-engine-service/main.go
+++ cmd/argus-stream-engine-service/main.go
@@ -7,6 +7,14 @@ import (
 func main() {
 	rtmp.LogInfo("Initializing Argus Stream Engine...")
 
+	config := NewConfig()
+	if config.WithRelay == "TRUE" {
+		rtmp.LogInfo("Relay enabled.")
+		rtmp.LogInfo("Relay Ingress Address: " + config.RelayIngressAddress)
+		rtmp.LogInfo("Relay Egress Address: " + config.RelayEgressAddress)
+		rtmp.LogInfo("Egress Destination Address: " + config.EgressDestinationAddress)
+	}
+
 	rtmpServer := rtmp.NewServer()
 	if rtmpServer != nil {
 		rtmpServer.Start()
