613c0461b8a9868c6b07cc82de8b0969f4120699
Gabe Susman <gabe.susman@gmail.com>
2024-12-14 11:59:54 -0700

    included Config fields and a new NewConfig method to load them

--- cmd/argus-stream-engine-service/config.go
+++ cmd/argus-stream-engine-service/config.go
@@ -1,4 +1,21 @@
 package main
 
+import "os"
+
+// Config encapsulates the fields used to configure the service.
 type Config struct {
+	WithRelay                string
+	RelayIngressAddress      string
+	RelayEgressAddress       string
+	EgressDestinationAddress string
+}
+
+// NewConfig constructs a new Configuration for the service.
+func NewConfig() Config {
+	return Config{
+		WithRelay:                os.Getenv("WITH_RELAY"),
+		RelayIngressAddress:      os.Getenv("RELAY_INGRESS_ADDRESS"),
+		RelayEgressAddress:       os.Getenv("RELAY_EGRESS_ADDRESS"),
+		EgressDestinationAddress: os.Getenv("EGRESS_DESTINATION_ADDRESS"),
+	}
 }
