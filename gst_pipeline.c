7dca50e780f25cf155ddc4dc9404776fd9f1ab8c
Max-Gabriel-Susman <gabe.susman@gmail.com>
2025-01-04 16:16:03 -0700

    cleaned up gst_pipeline.c

--- gst_pipeline.c
+++ gst_pipeline.c
@@ -2,7 +2,6 @@
 #include <gst/gst.h>
 #include <glib.h>
 
-// We'll keep a global pointer to the GMainLoop so we can stop it.
 static GMainLoop *loop = NULL;
 
 void init_gstreamer(void)
@@ -20,18 +19,14 @@ void start_rtmp_forwarding(const char *pipelineStr)
 
     loop = g_main_loop_new(NULL, FALSE);
 
-    // Attach a bus watch (optional, but useful for debugging or stopping on errors/EOS).
     GstBus *bus = gst_element_get_bus(pipeline);
     gst_bus_add_watch(bus, (GstBusFunc)gst_bus_async_signal_func, NULL);
     gst_object_unref(bus);
 
-    // Set pipeline to PLAYING
     gst_element_set_state(pipeline, GST_STATE_PLAYING);
 
-    // This blocks until we call stop_pipeline() or an error/EOS occurs.
     g_main_loop_run(loop);
 
-    // Cleanup after main loop finishes
     gst_element_set_state(pipeline, GST_STATE_NULL);
     gst_object_unref(pipeline);
 
